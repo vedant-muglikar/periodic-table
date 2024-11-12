1067f692992a15666fd6e8a328f968478b771a9f
Vedu-Vedant <vedantmuglikar1@gmail.com>
2024-11-12 21:39:12 +0530

    popup backend comp

--- app.js
+++ app.js
@@ -2,6 +2,41 @@ const closeBtn = document.querySelector(".popup-close");
 const elementCards = document.querySelectorAll(".element-box");
 const popupContainer = document.querySelector(".popup-container");
 const popup = document.querySelector(".popup");
+const eleImg = document.querySelector(".ele-img");
+const eleName = document.querySelector(".ele-name");
+const eleNo = document.querySelector(".ele-no");
+const eleMass = document.querySelector(".ele-mass");
+const eleConfig = document.querySelector(".ele-e-config");
+const eleDensity = document.querySelector(".ele-density");
+const eleCrust = document.querySelector(".ele-crust");
+const eleUni = document.querySelector(".ele-uni");
+
+let elements = [];
+
+function popupUpdate(
+  img,
+  name,
+  atNo,
+  atMass,
+  eConfig,
+  density,
+  crust,
+  universe,
+  element_name
+) {
+  elements.forEach((e) => {
+    if (e.name == element_name) {
+      img.src = e.img;
+      name.innerText = e.name;
+      atNo.innerText = e.atNo;
+      atMass.innerText = e.atMass;
+      eConfig.innerText = e.eConfig;
+      density.innerText = e.density;
+      crust.innerText = e.crust;
+      universe.innerText = e.universe;
+    }
+  });
+}
 
 /*working of popup */
 closeBtn.addEventListener("click", () => {
@@ -10,8 +45,39 @@ closeBtn.addEventListener("click", () => {
 });
 
 elementCards.forEach((card) => {
-  card.addEventListener("click", () => {
+  card.addEventListener("click", (e) => {
+    element_name = e.target.children[3].innerText;
+    popupUpdate(
+      eleImg,
+      eleName,
+      eleNo,
+      eleMass,
+      eleConfig,
+      eleDensity,
+      eleCrust,
+      eleUni,
+      element_name
+    );
     popupContainer.classList.add("active");
     popup.classList.add("active");
   });
 });
+
+/*Fetching data of elements from json and appending to elements*/
+
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    elements = data.map((element) => {
+      return {
+        img: element.img,
+        name: element.name,
+        atNo: element.atNo,
+        atMass: element.atMass,
+        eConfig: element.eConfig,
+        density: element.density,
+        crust: element.crust,
+        universe: element.universe,
+      };
+    });
+  });
