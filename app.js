6a19eebb3531f7dafda00c11ddfa301e588d264a
vedant-muglikar <vedantmuglikar1@gmail.com>
2024-11-21 22:02:01 +0530

    temp slider

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const eleConfig = document.querySelector(".ele-e-config");
 const eleDensity = document.querySelector(".ele-density");
 const eleCrust = document.querySelector(".ele-crust");
 const eleUni = document.querySelector(".ele-uni");
+const slider = document.querySelector(".temp-slider");
+const tempValue = document.querySelector(".temp-value");
 
 let elements = [];
 
@@ -38,6 +40,38 @@ function popupUpdate(
   });
 }
 
+/*temp value update*/
+function tempUpdate(temp) {
+  slider.value = temp;
+  tempValue.value = temp;
+}
+
+/*updating the color of elements */
+function colorUpdate(temp) {
+  elements.forEach((element) => {
+    name = element.name;
+    console.log(elementCards, name, element, element.boiling);
+    elementCards.forEach((box) => {
+      console.log(box);
+      if (box.classList.contains(name)) {
+        console.log(element.boiling);
+        /*gass*/
+        if (element.boiling < temp) {
+          box.style.background = "white";
+        }
+        /*liquid*/
+        if (element.boiling > temp && temp > element.melting) {
+          box.style.background = "blue";
+        }
+        /*solid*/
+        if (element.melting > temp) {
+          box.style.background = "red";
+        }
+      }
+    });
+  });
+}
+
 /*working of popup */
 closeBtn.addEventListener("click", () => {
   popupContainer.classList.remove("active");
@@ -63,6 +97,18 @@ elementCards.forEach((card) => {
   });
 });
 
+/*update the display and slider with temperature value*/
+slider.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  tempUpdate(e.target.value);
+  colorUpdate(e.target.value);
+});
+
+tempValue.addEventListener("input", (e) => {
+  tempUpdate(e.target.value);
+  colorUpdate(e.target.value);
+});
+
 /*Fetching data of elements from json and appending to elements*/
 
 fetch("./data.json")
@@ -78,6 +124,79 @@ fetch("./data.json")
         density: element.density,
         crust: element.crust,
         universe: element.universe,
+        boiling: element.boiling,
+        melting: element.melting,
       };
     });
   });
+
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/* */
+/*New code */
+/*youtuber code */
+let availablekeywords = [];
+
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    availablekeywords = data.map((element) => element.name);
+    // console.log(availablekeywords);
+  });
+/* */
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.querySelector("#input-box");
+
+inputBox.onkeyup = function () {
+  let result = [];
+  let input = inputBox.value;
+
+  if (input.length) {
+    result = availablekeywords.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(result);
+  }
+  display(result);
+
+  if (!result.length) {
+    resultBox.innerHTML = "";
+  }
+};
+
+function display(result) {
+  const content = result.map((list) => {
+    return "<li onClick=selectInput(this)>" + list + "</li>";
+  });
+
+  resultBox.innerHTML = "<ul>" + content.join("") + "</ul>";
+}
+
+function selectInput(list) {
+  const element_name = list.innerHTML;
+  inputBox.value = element_name;
+  resultBox.innerHTML = "";
+
+  popupUpdate(
+    eleImg,
+    eleName,
+    eleNo,
+    eleMass,
+    eleConfig,
+    eleDensity,
+    eleCrust,
+    eleUni,
+    element_name
+  );
+
+  popupContainer.classList.add("active");
+  popup.classList.add("active");
+}
+/*youtuber code */
+/*New code */
